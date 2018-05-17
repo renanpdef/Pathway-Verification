1bb6b368f65f3fe78189a09b9f3a52686cac6d91
renanpdef <34146660+renanpdef@users.noreply.github.com>
2018-05-17 02:12:49 -0300

    inacessibleStep2 DFS

--- src/main/java/parser/PathwayVerificationTest/FindInaccessibleStep2.java
+++ src/main/java/parser/PathwayVerificationTest/FindInaccessibleStep2.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solution;
@@ -23,42 +24,65 @@ public class FindInaccessibleStep2 extends SequenceParser {
 	//Return a set of Elements that is a inaccessible step.
 	//An element is a inaccessible step if there are no valid path to get to the element.
 	public List<Element> findInaccessibleSteps() {
-		List<Element> visitedElementList = new ArrayList<Element>();
 		List<Element> accessibleElements = new ArrayList<Element>();
-		List<Element> inaccessibleElements = new ArrayList<Element>();
+		List<Element> visitedElements = new ArrayList<Element>();
+		Stack<Element> elementsStack = new Stack<Element>();
+		List<Sequence> sequenceList = new ArrayList<Sequence>();
+
+		Element initialElement = pathway.getElement().get(0);
+		accessibleElements.add(initialElement);
+		visitedElements.add(initialElement);
+		elementsStack.push(initialElement);
 		
-		Element root = pathway.getElement().get(0);
-		accessibleElements.add(root);
-		visitedElementList.add(root);
-		Sequence sequence = root.getOutputSequences().get(0);
-		int pos = 0;
-		
-		while(visitedElementList.size() < pathway.getElement().size()) {
+		while(elementsStack.isEmpty() == false) {
 			Model model = new Model("Find All Solutions: " + 1); //Create a model to verify a valid path with ChocoSolver.
-			List<Sequence> sequenceList = new ArrayList<Sequence>();
-			sequenceList.add(sequence);
-			List<BoolVar> boolSequences = sequenceListToBoolVarList(model, sequenceList);//Get a list of BoolVar from a list of sequence of the mapElementOutputSequences.
-			
-			if (boolSequences!=null && !boolSequences.isEmpty()) {
-				for(int i = 0; i < boolSequences.size(); i++) {
-					model.arithm(boolSequences.get(i), "=", 1).post();//Post the constraint "the sequence have to be true" to the model.
-				}
-				if(model.getSolver().findSolution() != null) {
-					accessibleElements.add(sequence.getInputStep());
-					if(sequence.getInputStep().getOutputSequences() != null) {
-						sequence = sequence.getInputStep().getOutputSequences().get(pos);
-					}else {
-						pos++;
-						sequence = sequence.getOutputStep().getOutputSequences().get(pos);
-					}
-				}else {
-					inaccessibleElements.add(sequence.getInputStep());
+			Element element = elementsStack.lastElement();
+			int index = 0;
+			for(index = 0; index < element.getOutputSequences().size(); index++) {
+				Element inputStep = element.getOutputSequences().get(index).getInputStep();
+				if(!visitedElements.contains(inputStep)) {
+					visitedElements.add(inputStep);
+					break;
 				}
 			}
-			
+			if(index == element.getOutputSequences().size()) {
+				elementsStack.pop();
+				if(sequenceList.size() > 0) {
+					sequenceList.remove(sequenceList.size()-1);
+				}
+			}else {
+				Sequence sequence = element.getOutputSequences().get(index);
+				sequenceList.add(sequence);
+				List<BoolVar> boolSequences = sequenceListToBoolVarList(model, sequenceList);//Get a list of BoolVar from a list of sequence of the mapElementOutputSequences.
+				
+				if (boolSequences!=null && !boolSequences.isEmpty()) {
+					for(int j = 0; j < boolSequences.size(); j++) {
+						model.arithm(boolSequences.get(j), "=", 1).post();//Post the constraint "the sequence have to be true" to the model.
+					}
+					if(model.getSolver().findSolution() != null) {
+						accessibleElements.add(sequence.getInputStep());
+						if(sequence.getInputStep().getOutputSequences() != null) {
+							elementsStack.push(sequence.getInputStep());
+						}else {
+							sequenceList.remove(sequenceList.size()-1);
+						}
+					}else {
+						sequenceList.remove(sequenceList.size()-1);
+					}
+				}
+			}
 		}
-		
-		return null;
+		return getInaccessibleElements(pathway.getElement(), accessibleElements);
+	}
+
+	private List<Element> getInaccessibleElements(List<Element> elements, List<Element> accessibleElements) {
+		List<Element> inaccessibleElements = new ArrayList<Element>();
+		for(int i = 0; i < elements.size(); i++) {
+			if(!accessibleElements.contains(elements.get(i))) {
+				inaccessibleElements.add(elements.get(i));
+			}
+		}
+		return inaccessibleElements;
 	}
 
 }
